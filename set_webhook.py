e62e0e2f843b5b17a0714986bbcdea3ce246c9d9
gulomovich <162092231+gulomovich@users.noreply.github.com>
2025-07-01 16:58:17 +0500

    Create set_webhook.py

---
+++ set_webhook.py
@@ -0,0 +1,12 @@
+# set_webhook.py
+import asyncio
+from bot import bot
+
+WEBHOOK_URL = "https://your-app-name.onrender.com/"  # Render domainingizni yozing
+
+async def main():
+    await bot.set_webhook(WEBHOOK_URL)
+    print("Webhook muvaffaqiyatli o‘rnatildi:", WEBHOOK_URL)
+
+if __name__ == "__main__":
+    asyncio.run(main())
